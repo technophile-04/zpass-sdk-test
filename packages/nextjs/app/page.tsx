ea3938f7acffdc311e809e43b401f1b381ede1b2
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-11-08 22:15:09 +0800

    style rewards page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -310,7 +310,7 @@ const Home = () => {
                 </div>
               </div>
               <div className="absolute bottom-0 left-0 right-0 card w-full bg-base-200 rounded-none">
-                <div className="p-4">
+                <div className="p-3">
                   <TokensRewards rewards={squeezeReward} />
                 </div>
               </div>
--- packages/nextjs/app/rewards/page.tsx
+++ packages/nextjs/app/rewards/page.tsx
@@ -1,38 +1,42 @@
 "use client";
 
 import { useAccount } from "wagmi";
-import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { TokensBalances } from "~~/components/TokensBalances";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 
 const Rewards = () => {
-  const { address: connectedAddress } = useAccount();
+  const { isConnected } = useAccount();
 
-
-  if (!connectedAddress) {
+  if (!isConnected) {
     return (
-      <main className="flex min-h-screen flex-col items-center p-8">
-        <div className="z-10 max-w-5xl w-full flex flex-row gap-8 items-center justify-between">
-          <RainbowKitCustomConnectButton />
-          <img src="/priest.jpg" width="550" />
+      <main>
+        <div
+          className="flex justify-center items-center min-h-screen bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url('/priest.jpg')`,
+          }}
+        >
+          <div className="z-10 place-content-center place-items-center">
+            <RainbowKitCustomConnectButton />
+          </div>
         </div>
       </main>
     );
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="z-10 max-w-5xl w-full flex flex-row gap-8 items-center justify-between">
-        <div className="flex flex-col space-y-4">
-          <div className="card w-full bg-base-200 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title text-2xl font-bold text-primary">Rewards Balance</h2>
-              <div className="divider"></div>
-              <TokensBalances />
-            </div>
+    <main>
+      <div
+        className="relative flex justify-center items-center min-h-screen bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url('/priest.jpg')`,
+        }}
+      >
+        <div className="absolute bottom-0 left-0 right-0 card w-full bg-base-200 rounded-none">
+          <div className="p-3">
+            <TokensBalances />
           </div>
         </div>
-
-        <img src="/priest.jpg" width="550" />
       </div>
     </main>
   );
